7f14487e9b10f5402d4acb2a1a6467d1fda43820
KothaShashank812 <kothashashank30@gmail.com>
2025-09-24 15:14:20 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,17 @@
+
+
+public class Sum {
+    public static void main(String[] args) {
+      
+
+        
+        int a = 2;
+        int b = 3;
+
+        
+        int sum = a + b;
+
+        
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}
